60b770a7497451ac64942d601d903b0d076381b3
eugenio martinez <emartinez714@gmail.com>
2026-04-06 15:41:01 -0400

    fix shebang

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 type-annotatted function add
 """
 def add(a: float, b: float) -> float:
     """
-    adds two floats and returns the result
+    adds two floats and returns sum the result
     """
     return a + b
