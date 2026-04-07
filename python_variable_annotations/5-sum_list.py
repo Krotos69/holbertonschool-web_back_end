ddf9642e59c643073f1d5a4e168fe4053de62786
eugenio martinez <emartinez714@gmail.com>
2026-04-06 23:27:44 -0400

    fix leng of char

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-type-annotatted function sum_list that takes a list input_list of floats as argument and returns their sum as a float.
+type-annotatted function sum_list that takes a list input_list of
+floats as argument and returns their sum as a float.
 """
 
 from typing import List
