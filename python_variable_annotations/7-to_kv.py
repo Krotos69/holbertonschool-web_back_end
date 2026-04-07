e2fa2dc751a63d8a03707fc63de56ec4ae1001dc
eugenio martinez <emartinez714@gmail.com>
2026-04-07 11:08:15 -0400

    missign spaces

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -7,9 +7,10 @@ values with the appropriate types.
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int,float]) -> Tuple[str, float]:
-    """Returns a tuple where:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Returns a tuple where:
     - the first element is the string k
     - the second element is the square of v as a float(string)
     """
-    return (k, float(v **2))
+    return (k, float(v ** 2))
