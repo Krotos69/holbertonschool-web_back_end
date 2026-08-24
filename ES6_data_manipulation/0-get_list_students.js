f86267cf01c03bde69b8fd97c8aba1a7a19de5d3
eugenio martinez <emartinez714@gmail.com>
2026-08-24 19:08:05 -0400

    Basic list of objects

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,11 @@
+// Function: getListStudents
+// Returns: an array of student objects
+// Each object contains: id (Number), firstName (String), location (String)
+
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
