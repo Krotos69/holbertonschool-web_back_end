f6fd82970e75208fefb93d265c62d6efa22487eb
eugenio martinez <emartinez714@gmail.com>
2026-08-23 22:17:48 -0400

    Primitive - Holberton Class

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,32 @@
+// 8-hbtn_class.js
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw new TypeError('Size must be a number');
+    }
+    if (typeof location !== 'string') {
+      throw new TypeError('Location must be a string');
+    }
+
+    this._size = size;
+    this._location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  // Cast to Number → return size
+  valueOf() {
+    return this._size;
+  }
+
+  // Cast to String → return location
+  toString() {
+    return this._location;
+  }
+}
