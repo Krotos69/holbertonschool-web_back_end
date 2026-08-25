e4833042419a99fc5e45302f0f647ee8ed860b17
eugenio martinez <emartinez714@gmail.com>
2026-08-24 21:17:17 -0400

    Set data structure

---
+++ ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+// Function: setFromArray
+// Purpose: Return a Set created from an array
+// Params:
+//   array (Array) - list of elements of any type
+// Returns: Set containing the array's elements
+
+export default function setFromArray(array) {
+  return new Set(array);
+}
