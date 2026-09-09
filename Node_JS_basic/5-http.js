5197a5c8216171707aa26a60b092e808f81a1a3e
eugenio martinez <emartinez714@gmail.com>
2026-09-09 17:54:21 -0400

    Create a more complex HTTP server using Node's HTTP module

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,65 @@
+const http = require('http');
+const fs = require('fs');
+
+const database = process.argv[2];
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (error, data) => {
+      if (error) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data
+        .split('\n')
+        .map((line) => line.trim())
+        .filter((line) => line.length > 0);
+
+      const students = lines.slice(1);
+      const fields = {};
+
+      students.forEach((student) => {
+        const studentData = student.split(',');
+        const firstName = studentData[0];
+        const field = studentData[3];
+
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+
+        fields[field].push(firstName);
+      });
+
+      let output = `Number of students: ${students.length}`;
+
+      Object.keys(fields).forEach((field) => {
+        output += `\nNumber of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`;
+      });
+
+      resolve(output);
+    });
+  });
+}
+
+const app = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  if (request.url === '/') {
+    response.end('Hello Holberton School!');
+  } else if (request.url === '/students') {
+    countStudents(database)
+      .then((studentsInfo) => {
+        response.end(`This is the list of our students\n${studentsInfo}`);
+      })
+      .catch((error) => {
+        response.end(`This is the list of our students\n${error.message}`);
+      });
+  } else {
+    response.end('Hello Holberton School!');
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
