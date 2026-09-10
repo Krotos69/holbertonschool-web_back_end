bf35fc63b25fe8c636e24a85dbb9f6d82072d4ce
eugenio martinez <emartinez714@gmail.com>
2026-09-10 13:49:05 -0400

    correct code

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,9 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
+process.stdin.setEncoding('utf8');
+
 process.stdin.on('data', (data) => {
-  const input = data.toString().trim();
-  process.stdout.write(`Your name is: ${input}`);
+  process.stdout.write(`Your name is: ${data}`);
 });
 
 process.stdin.on('end', () => {
